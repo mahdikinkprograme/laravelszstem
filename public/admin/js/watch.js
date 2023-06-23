e9ecee59e044c16ccf098e2c0e44fd2f5079c1bc
mahdikinkprograme <106489688+mahdikinkprograme@users.noreply.github.com>
2023-06-23 19:15:44 +0200

    Update watch.js

--- public/admin/js/watch.js
+++ public/admin/js/watch.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
-   // $('.addcart').click(function(e){
-   //     e.preventDefault();
-   //    var idprod = $(this).closest('.deletall').find('.prod_id').val();
-   //     console.log(idprod)
-   // })
-//
-   // $.ajax({
-   //     type: 'POST',  
-   //     data: { idprod : 'idprod' },  
-   //     headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-   //     success: function (response) {
-   //         //$('.deletall').html(response.status);
-//
-   //     },
-   //    
-   // });
+    $('.delcart').click(function(e){
+        e.preventDefault();
+       var prod_id = $(this).data('cartid');
+       console.log(prod_id);
+
+       $.ajax({
+        method: 'DELETE',
+        url : '/deletcart',  
+        data: { 'prod_id' : prod_id },  
+        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+        success: function (response) {
+            (response.status);
+            window.location.reload();
+           // $('.delcart').find('.prod_id').html(response.status);
+
+        },
+    });
+       
+    });
 
 });

e0a1037b43f20bd6b257dc99ea9058519a8e84b4
mahdikinkprograme <106489688+mahdikinkprograme@users.noreply.github.com>
2023-06-27 22:47:32 +0200

    Update detail.blade.php

--- resources/views/student/detail.blade.php
+++ resources/views/student/detail.blade.php
@@ -1,43 +1,111 @@
 @include('student.include')
 
-
-@if(session('succes'))
-     <div class="alert alert-success">{{session('succes')}}</div>
-        @endif
+                <!--<button><a class="button home__button" href="{{ url('carts/'.$product->id) }}">add to cart</a></button>-->
+<!DOCTYPE html>
+   <html>
+     <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <title>Product Card/Page</title>
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="{{asset('admin/css/detail.css')}}">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+     </head>
+     <body>
         
-<section class="home" id="home">
-    <div class="home__container container grid">
-        <div class="home__img-bg">
-            <img src="{{asset('admin/img/'.$product->image)}}" alt="" class="home__img">
-        </div>
-        <div class="home__social">
-            <a href="https://www.facebook.com/" target="_blank" class="home__social-link">
-                Facebook
-            </a>
-            <a href="https://twitter.com/" target="_blank" class="home__social-link">
-                Twitter
-            </a>
-            <a href="https://www.instagram.com/" target="_blank" class="home__social-link">
-                Instagram
-            </a>
-        </div>
-       
-        <div class="home__data">
-            <h1 class="home__title">NEW WATCH <br> COLLECTIONS B720</h1>
-            <p class="home__description">
-                Latest arrival of the new imported watches of the B720 series, 
-                with a modern and resistant design.
-            </p>
-            <span class="home__price">{{$product->name}}</span>
-            <div class="home__btns">
-                <a href="#" class="button button--gray button--small">
-                    {{$product->price}}
-                </a>
-                <button><a class="button home__button" href="{{ url('carts/'.$product->id) }}">add to cart</a></button>
+    <div class = "card-wrapper">
+      <div class = "card">
+        <!-- card left -->
+        <div class = "product-imgs">
+          <div class = "img-display">
+            <div class = "img-showcase">
+              <img src = "{{asset('admin/img/'.$product->image)}}" alt = "shoe image">
+              
             </div>
-     </div> 
-    </div>
+          </div>
+          <div class = "img-select">
+            <div class = "img-item">
+              <a href = "#" data-id = "1">
+                <img src = "{{asset('admin/img/home.png')}}" alt = "shoe image">
+              </a>
+            </div>
+            <div class = "img-item">
+              <a href = "#" data-id = "2">
+                <img src = "shoes_images/shoe_2.jpg" alt = "shoe image">
+              </a>
+            </div>
+            <div class = "img-item">
+              <a href = "#" data-id = "3">
+                <img src = "shoes_images/shoe_3.jpg" alt = "shoe image">
+              </a>
+            </div>
+            <div class = "img-item">
+              <a href = "#" data-id = "4">
+                <img src = "shoes_images/shoe_4.jpg" alt = "shoe image">
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- card right -->
+        <div class = "product-content">
+          <h2 class = "product-title">nike shoes</h2>
+          <a href = "#" class = "product-link">visit nike store</a>
+          <div class = "product-rating">
+            <i class = "fas fa-star"></i>
+            <i class = "fas fa-star"></i>
+            <i class = "fas fa-star"></i>
+            <i class = "fas fa-star"></i>
+            <i class = "fas fa-star-half-alt"></i>
+            <span>4.7(21)</span>
+          </div>
 
-    
-</section>
-<!--==================== FEATURED ====================-->
+          <div class = "product-price">
+            <p class = "last-price">Old Price: <span>{{$product->price}}</span></p>
+            <p class = "new-price">New Price: <span>$249.00 (5%)</span></p>
+          </div>
+          @if(session('succes'))
+       <div class="alert alert-warning alert-dismissible fade show" role="alert">{{session('succes')}}
+        </div>
+        @endif
+          <div class = "product-detail">
+            <h2>about this item: </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eveniet veniam tempora fuga tenetur placeat sapiente architecto illum soluta consequuntur, aspernatur quidem at sequi ipsa!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, perferendis eius. Dignissimos, labore suscipit. Unde.</p>
+            <ul>
+              <li>Color: <span>Black</span></li>
+              <li>Available: <span>in stock</span></li>
+              <li>Category: <span>Shoes</span></li>
+              <li>Shipping Area: <span>All over the world</span></li>
+              <li>Shipping Fee: <span>Free</span></li>
+            </ul>
+          </div>
+          <form action= "{{ url('carts/'.$product->id) }}" method="GET">@csrf
+          <div class = "purchase-info">
+            <input type = "number" min = "0" value = "1" name="prod_qty" class="quantity">
+            <button><a class="button home__button">add to cart</a></button>
+            <button type = "button" class = "btn">Compare</button>
+          </div>
+          </form>
+          <div class = "social-links">
+            <p>Share At: </p>
+            <a href = "#">
+              <i class = "fab fa-facebook-f"></i>
+            </a>
+            <a href = "#">
+              <i class = "fab fa-twitter"></i>
+            </a>
+            <a href = "#">
+              <i class = "fab fa-instagram"></i>
+            </a>
+            <a href = "#">
+              <i class = "fab fa-whatsapp"></i>
+            </a>
+            <a href = "#">
+              <i class = "fab fa-pinterest"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
